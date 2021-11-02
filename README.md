c8bc1a997f68e57e8a78793157d1cdab1a7041bd
DiegoP <diego.pozog@hotmail.com>
2021-11-01 23:18:45 -0500

    agregue nuevo codigo

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Noche-de-JavaScript
+# taller de javascript
+-Primer paso: Definir las formulas
+-Segundo paso: Implementar las formulas en javascript
+-Tercer paso: Crear funciones
+-Cuarto paso: Integrar JS con HTML
--- figuras.js
+++ figuras.js
@@ -1,1 +1,72 @@
-console.log("hola estimado Diego como estas?")
+
+console.group("Cuadrados");
+
+
+function perimetroCuadrado(lado) {
+  return lado * 4;
+}
+
+
+function areaCuadrado(lado) {
+  return lado * lado;
+}
+
+console.groupEnd();
+
+
+console.group("Triángulos");
+
+
+
+function perimetroTriangulo(lado1, lado2, base) {
+  return lado1 + lado2 + base;
+}
+
+
+function areaTriangulo(base, altura) {
+  return (base * altura) / 2;
+}
+console.log("El área del triángulo es: " + areaTriangulo + "cmˆ2");
+
+console.groupEnd();
+
+
+console.group("Círculos");
+
+function diametroCirculo(radio) {
+  return radio * 2;
+}
+
+
+const PI = Math.PI;
+console.log("PI es: " + PI);
+
+
+function perimetroCirculo(radio) {
+  const diametro = diametroCirculo(radio);
+  return diametro * PI;
+}
+
+
+function areaCirculo(radio) {
+  return (radio * radio) * PI;
+}
+
+console.groupEnd();
+
+
+
+function calcularPerimetroCuadrado() {
+  const input = document.getElementById("InputCuadrado");
+  const value = input.value;
+
+  const perimetro = perimetroCuadrado(value);
+  alert(perimetro);
+}
+function calcularAreaCuadrado() {
+  const input = document.getElementById("InputCuadrado");
+  const value = input.value;
+
+  const area = areaCuadrado(value);
+  alert(area);
+}
